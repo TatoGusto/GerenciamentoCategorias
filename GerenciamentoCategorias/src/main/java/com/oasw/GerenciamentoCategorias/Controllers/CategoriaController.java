0f4ed297b78698c55c359c94a357a5c2969b8ffe
OtavioAugusto <santostatogusto@gmail.com>
2026-03-22 16:39:18 -0300

    controller criado

---
+++ GerenciamentoCategorias/src/main/java/com/oasw/GerenciamentoCategorias/Controllers/CategoriaController.java
@@ -0,0 +1,45 @@
+package com.oasw.GerenciamentoCategorias.Controllers;
+
+import com.oasw.GerenciamentoCategorias.Models.CategoriaModel;
+import com.oasw.GerenciamentoCategorias.Services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/categorias")
+public class CategoriaController {
+
+    @Autowired
+    CategoriaService categoriaService;
+
+    @PostMapping
+    public ResponseEntity<CategoriaModel> create(CategoriaModel categoriaModel){
+        CategoriaModel request = categoriaService.create(categoriaModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(categoriaModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaModel>> getAll(){
+        List<CategoriaModel> request = categoriaService.getAll();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaModel> getById(@PathVariable Long id){
+        CategoriaModel request = categoriaService.getById(id);
+        return ResponseEntity.ok().body(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        categoriaService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
