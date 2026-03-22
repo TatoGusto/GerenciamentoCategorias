f3de4d83104dce82762fe5adb0fc5f1d2a1b3ac1
OtavioAugusto <santostatogusto@gmail.com>
2026-03-22 16:19:31 -0300

    model criado

---
+++ GerenciamentoCategorias/src/main/java/com/oasw/GerenciamentoCategorias/GerenciamentoCategoriasApplication.java
@@ -0,0 +1,13 @@
+package com.oasw.GerenciamentoCategorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoCategoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoCategoriasApplication.class, args);
+	}
+
+}
